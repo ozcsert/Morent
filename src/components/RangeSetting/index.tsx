937eb61ca9c708e0a60df233eebd8ba8dbe1b376
palaogluserdest-dev <palaogluserdest@hotmail.com>
2024-09-20 10:38:23 +0300

    [SE-8] Added to switch range area function

--- src/components/RangeSetting/index.tsx
+++ src/components/RangeSetting/index.tsx
@@ -1,20 +1,40 @@
+"use client";
 import "./style.scss";
 import RaceArea from "./RaceArea";
 import SwapButton from "../../app/images/swap-button.svg";
 import Image from "next/image";
+import { useState } from "react";
 
 const RangeSettings = () => {
+  const [isSwitched, setIsSwitched] = useState<boolean>(false);
+
+  const handleSwitchRangeArea = () => {
+    setIsSwitched((prev) => !prev);
+  };
+
   return (
     <>
       <div className="rs-container">
         <div className="rs-pick-up">
-          <RaceArea title="Pick - Up" dateType="start" />
+          <RaceArea
+            title={`${!isSwitched ? "Pick - Up" : "Drop - Off"}`}
+            dateType={`${!isSwitched ? "start" : "finish"}`}
+          />
         </div>
         <div className="rs-switch-btn">
-          <Image src={SwapButton} alt="" width={24} height={24} />
+          <Image
+            src={SwapButton}
+            alt=""
+            width={24}
+            height={24}
+            onClick={handleSwitchRangeArea}
+          />
         </div>
         <div className="rs-drop-off">
-          <RaceArea title="Dorp - Off" dateType="finish" />
+          <RaceArea
+            title={`${!isSwitched ? "Drop - Off" : "Pick - Up"}`}
+            dateType={`${!isSwitched ? "finish" : "start"}`}
+          />
         </div>
       </div>
     </>
