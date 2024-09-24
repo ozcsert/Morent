6ffcb9d7c3f449f627cd07ffc90ca6559b95bf94
fatihsimssek <mm.fatihsimsek@gmail.com>
2024-09-24 23:34:23 +0300

    [SE-18] - Logo component silindi footer component düzenlendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,10 @@
-
 import "./page.scss";
-import Link from "next/link";
 import Footer from "@/components/Footer";
-import Logo from "@/components/Logo";
-
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="footer">
-
-      <Logo text='MORENT'/>
-      <div className="footer-menu">
-      <div className="footer-about">
-        <Link className="footer-link" href="#">About</Link>
-        <Footer url="#" text="How it works"/>
-        <Footer url="#" text="Featured"/>
-        <Footer url="#" text="Partnership"/>
-        <Footer url="#" text="Bussiness Relation"/>
-        </div>
-        <div className="footer-community">
-        <Link className="footer-link" href="#">Community</Link>
-        <Footer url="#" text="Events"/>
-        <Footer url="#" text="Blog"/>
-        <Footer url="#" text="Podcast"/>
-        <Footer url="#" text="Invite a friend"/>
-        </div>
-      <div className="footer-socails">
-
-      <Link className="footer-link" href="#">Socials</Link>
-      <Footer url="//discord.com" text="Discord" />
-      <Footer url="//instagram.com" text="Instagram" />
-      <Footer url="//twitter.com" text="Twitter" />
-      <Footer url="//facebook.com" text="Facebook" />
-      </div>
-      </div>
-      
-      </div>
-      <div className="line"></div>
-      <div className="footer-bottom">
-
-      <Link href="#">©2022 MORENT. All rights reserved</Link>
-      <div className="footer-privacy">
-
-      <Link href="#">Privacy & Policy</Link>
-      <Link href="#">Terms & Condition</Link>
-      </div>
-
-      </div>
-        
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,62 @@
-import React from 'react'
-import Link from 'next/link'
-import './styles.scss'
+import React from "react";
+import Link from "next/link";
+import "./styles.scss";
 
-
-
-
-type FooterProps = {
-  text: string,
-  url: string
-}
-
-const Footer = ({ url, text }: FooterProps) => {
-
+const Footer = () => {
   return (
     <>
-      <footer className='footer-container'>
-        <ul className='footer-list'>
-        <Link href={url}>{text}</Link>
+      <footer className="footer-container">
+        <ul className="footer-list">
+          <div className="footer">
+            <div className="logo">
+              <h1>MORENT</h1>
+              <p>
+                Our vision is to provide convenience and help increase your
+                sales business.
+              </p>
+            </div>
+            <div className="footer-menu">
+              <div className="footer-about">
+                <Link className="footer-link" href="#">
+                  About
+                </Link>
+                <Link href="#">How it works</Link>
+                <Link href="#">Featured</Link>
+                <Link href="#">Partnership</Link>
+                <Link href="#">Bussiness Relation</Link>
+              </div>
+              <div className="footer-community">
+                <Link className="footer-link" href="#">
+                  Community
+                </Link>
+                <Link href="#">Events</Link>
+                <Link href="#">Blog</Link>
+                <Link href="#">Podcast</Link>
+                <Link href="#">Invite a friend</Link>
+              </div>
+              <div className="footer-socails">
+                <Link className="footer-link" href="#">
+                  Socials
+                </Link>
+                <Link href="//discord.com">Discord</Link>
+                <Link href="//instagram.com">Instagram</Link>
+                <Link href="//twitter.com">Twitter</Link>
+                <Link href="//facebook.com">Facebook</Link>
+              </div>
+            </div>
+          </div>
+          <div className="line"></div>
+          <div className="footer-bottom">
+            <Link href="#">©2022 MORENT. All rights reserved</Link>
+            <div className="footer-privacy">
+              <Link href="#">Privacy & Policy</Link>
+              <Link href="#">Terms & Condition</Link>
+            </div>
+          </div>
         </ul>
-
       </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
