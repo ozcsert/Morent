f99c64ea2a9be52911c49087e055074e4b3f97bb
esravar <varesra@hotmail.com>
2024-10-07 19:05:51 +0300

    [SE-3] Hamburger menu added..

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,27 @@
 "use client";
 import './styles.scss';
-import HeartButton from "../../app/images/heartheart.svg";
-import BellButton from "../../app/images/bell.svg";
-import NotificationButton from "../../app/images/notificationbell.svg";
-import ProfileButton from "../../app/images/Imageprofile.svg";
 import Image from "next/image";
 import useDeviceSize from './size'; 
+import { useState } from 'react';
+import { motion, Variants } from "framer-motion";
+
 
 const Navbar: React.FC = () => {
   const [width, height] = useDeviceSize();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+const toggleMenu = () => {
+    setIsOpen(!isOpen);
+};
+const itemVariants: Variants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 }
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+};
+
   return (
     <div>
     {width>780 ? ( 
@@ -77,12 +90,20 @@ const Navbar: React.FC = () => {
     <header className="header">
     {/* Left: Hamburger Icon */}
     <div className='first-row'>
-      <div className="left-section">
+      <div className={`hamburger-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
         <button className="menu-icon">
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
         </button>
+        <nav className={`menu ${isOpen ? 'open' : ''}`}>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
       </div>
 
       {/* Right: Profile Picture */}
