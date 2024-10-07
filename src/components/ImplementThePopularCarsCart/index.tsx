07623664d77d4f87fc7859f06ef77e356a9a93f1
onx66 <oipek01@mail.com>
2024-10-07 19:16:07 +0300

    [SE-12] - Implement-the-car

--- src/components/ImplementThePopularCarsCart/index.tsx
+++ src/components/ImplementThePopularCarsCart/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import './style.scss';
 import like from "../../assets/like.png";
+import likeRed from "../../assets/likeRed.png";
 import gasStation from "../../assets/gas-station.png";
 import circle from "../../assets/circle icon.png";
 import users from "../../assets/profile-2user.png"
@@ -13,13 +14,26 @@ import { dummyCars } from "../../constants/index";
 
 const ImplementThePopularCarsCart = () => {
     const popularCarArray = dummyCars.slice(0, 4);
+
+    const image1 =  like.src ;
+    const image2 = likeRed.src ;
+    let isImage1 = true;
+    function changeLikeColor() {
+        const likeImg = document.getElementById('like');
+        if (isImage1) {
+            likeImg.src = image2;
+        } else {
+            likeImg.src = image1;
+        }
+        isImage1 = !isImage1;
+    }
     return (
         <> {popularCarArray.map((info, index) => (
             <div className='popular-car-cart' key={index}>
                 <div className='popular-car-main'>
                     <div className='popular-car-cart-header-title'>
                         <h2 className='popular-car-cart-name'>{info.name}</h2>
-                        <button className='popular-car-cart-header-title-like'><img src={like.src} width={24} height={24} /></button>
+                        <img src={like.src} width={24} height={24} id='like' className='popular-car-cart-header-title-like' onClick={changeLikeColor} />
                     </div>
                     <div className='popular-car-cart-header-subtitle'>
                         <p>{info.type}</p>
@@ -40,7 +54,7 @@ const ImplementThePopularCarsCart = () => {
                         </div>
                     </div>
                     <div className='popular-car-cart-footer'>
-                        <div className='popular-car-cart-price'><span>{info.price}/</span>  day</div>
+                        <div className='popular-car-cart-price'><span>{info.price}/</span>day</div>
                         <button className='popular-car-cart-btn'>Rent Now</button>
                     </div>
                 </div>
