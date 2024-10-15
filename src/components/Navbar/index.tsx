29dbfc68a21d69264b5a541088302a003f43bb6c
esravar <varesra@hotmail.com>
2024-10-15 19:11:26 +0300

    [SE-3] Animated hamburger menu

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,20 +3,19 @@ import './styles.scss';
 import Image from "next/image";
 import useDeviceSize from './size'; 
 import { useState } from 'react';
-import Link from "next/link";
-import { slide as Menu } from "react-burger-menu";
+import { useClickAway } from "react-use";
+import { useRef } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import Hamburger from "hamburger-react";
+import { routes } from "../../types/routes";
 
 const Navbar: React.FC = () => {
   const [width, height] = useDeviceSize();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setOpen] = useState<boolean>(false);
 
-  const handleIsOpen = () => {
-    setIsOpen(!isOpen);
-  };
+  const ref = useRef(null);
 
-  const closeSideBar = () => {
-    setIsOpen(false);
-  };
+  useClickAway(ref, () => setOpen(false));
 
   return (
     <div>
@@ -88,31 +87,14 @@ const Navbar: React.FC = () => {
     </nav>
     
   ):(
-    <><header className="header">
-            {/* Left: Hamburger Icon */}
-            <div className='first-row'>
-              <Menu className="base-menu" isOpen={isOpen} onOpen={handleIsOpen} onClose={handleIsOpen}>
-                <Link className="menu-item" href="/home" onClick={closeSideBar}>
-                  Home
-                </Link>
-
-                <Link className="menu-item" href="/about" onClick={closeSideBar}>
-                  About
-                </Link>
-
-                <Link className="menu-item" href="/services" onClick={closeSideBar}>
-                  Services
-                </Link>
-
-                <Link className="menu-item" href="/contact" onClick={closeSideBar}>
-                  Contact
-                </Link>
-              </Menu>
-
-           
-
-            {/* Right: Profile Picture */}
-            <div className="right-section">
+    <div>
+      <div ref={ref} className="first-row">
+      <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+      
+    
+ 
+  {/* Right: Profile Picture */}
+  <div className="right-section">
               <div className="profile-picture-mini">
                 <Image
                   src="/images/Imageprofile.svg"
@@ -122,6 +104,49 @@ const Navbar: React.FC = () => {
               </div>
             </div>
           </div>
+          <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20"
+          >
+            <ul className="grid gap-2">
+              {routes.map((route, idx) => {
+                const { Icon } = route;
+
+              return (
+                <motion.li
+                  initial={{ scale: 0, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 20,
+                    delay: 0.1 + idx / 10,
+                  }}
+                  key={route.title}
+                  className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700"
+                >
+                  <a
+                    onClick={() => setOpen((prev) => !prev)}
+                    className={
+                      "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
+                    }
+                    href={route.href}
+                  >
+                    <span className="flex gap-1 text-lg">{route.title}</span>
+                    <Icon className="text-xl" />
+                  </a>
+                </motion.li>
+              );
+            })}
+          </ul>
+        </motion.div>
+      )}
+    </AnimatePresence>
              <div>
               <div className="logo-mini">
                 <h1 className="logo-text">MORENT</h1>
@@ -164,8 +189,8 @@ const Navbar: React.FC = () => {
             </div>
             )}
    
-  </header>
-  </>
+  </div>
+
 )}
 </div>
 )};
