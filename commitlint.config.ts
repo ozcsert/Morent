7db0fd78ce491a9affa4d861b220dc356acb31af
palaogluserdest-dev <palaogluserdest@hotmail.com>
2024-10-16 14:20:51 +0300

    [SE-39]: Added husky and commitlint libraries

---
+++ commitlint.config.ts
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [],
+  rules: {
+    'type-enum': [0],
+    'header-max-length': [2, 'always', 72],
+    'subject-empty': [0],
+    'custom-format': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'custom-format': ({ header }: { header: string }) => {
+          const regex = /^(\[SE-\d{1,}\]:) (.{15,}$)/;
+          const match = header.match(regex);
+          console.log('header: ', header);
+
+          console.log(match);
+
+          if (!match) {
+            return [false, 'Header format must be like "[SE-01]: Subject"'];
+          }
+
+          const subject = match[2];
+
+          if (!subject || subject.trim().length < 15) {
+            return [false, 'Subject may not be less than 15 characters '];
+          }
+
+          return [true];
+        },
+      },
+    },
+  ],
+};
