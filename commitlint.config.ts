13d4ce1c7fe70df7585d54defc0c1b045a056b94
palaogluserdest-dev <palaogluserdest@hotmail.com>
2024-10-18 20:52:34 +0300

    [SE-39] - Clear console.log 's

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,25 +3,24 @@ module.exports = {
   rules: {
     'type-enum': [0],
     'header-max-length': [2, 'always', 72],
-    'subject-empty': [],
+    'subject-empty': [0],
     'custom-format': [2, 'always'],
   },
   plugins: [
     {
       rules: {
         'custom-format': ({ header }: { header: string }) => {
-          const regex = /^(\[SE-\d{1,}\] - ) (.+$)/;
+          const regex = /^(\[SE-\d{1,}\] -) (.+)$/;
           const match = header.match(regex);
-          console.log('match', match);
 
           if (!match) {
-            return [false, 'Header format must be like "[SE-01]: Subject"'];
+            return [false, 'Header format must be like "[SE-01] - Subject"'];
           }
 
           const subject = match[2];
 
-          if (!subject || subject.trim().length < 15) {
-            return [false, 'Subject may not be less than 15 characters '];
+          if (!subject || subject.trim().length < 10) {
+            return [false, 'Subject may not be less than 10 characters '];
           }
 
           return [true];
