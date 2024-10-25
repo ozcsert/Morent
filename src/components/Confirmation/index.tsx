babd2de4430674595b645ede7f0f383d2c37e8b0
fatihsimssek <mm.fatihsimsek@gmail.com>
2024-10-25 07:07:59 +0300

    [SE-34] - Create-the-Confirmation-Component

--- src/components/Confirmation/index.tsx
+++ src/components/Confirmation/index.tsx
@@ -1,5 +1,8 @@
 import { PaymentFormValues } from '@/types/typeList';
 import { UseFormHandleSubmit } from 'react-hook-form';
+import Security from '@/app/images/payment/Security.svg';
+
+import './styles.scss';
 
 type ConfirmationProps = {
   handleSubmit: UseFormHandleSubmit<PaymentFormValues>;
@@ -11,8 +14,64 @@ const Confirmation: React.FC<ConfirmationProps> = ({
   onSubmit,
 }) => {
   return (
-    <div>
-      <button onClick={handleSubmit(onSubmit)}>Confirm Payment</button>
+    <div className="confirmation-container">
+      <div className="confirmation-container-title">
+        <h2>Confirmation</h2>
+        <div className="confirmation-subinfo">
+          <p>
+            We are getting to the end. Just few clicks and your rental is ready!
+          </p>
+          <p>Step 4 of 4</p>
+        </div>
+      </div>
+      <form className="confirmation-form">
+        <div className="confirmation-option">
+          <div className="confirmation-option-name">
+            <div>
+              <input
+                type="checkbox"
+                id="agreed"
+                name="agreement"
+                value="agreed"
+              />
+              <label htmlFor="agreed">
+                I agree with sending an Marketing and newsletter emails. No
+                spam, promissed!
+              </label>
+            </div>
+          </div>
+        </div>
+      </form>
+
+      <form className="confirmation-form">
+        <div className="confirmation-option">
+          <div className="confirmation-option-name">
+            <div>
+              <input type="checkbox" id="terms" name="terms-agreement" />
+              <label htmlFor="terms">
+                I agree with our terms and conditions and privacy policy.
+              </label>
+            </div>
+          </div>
+        </div>
+      </form>
+      <button className="confirmation-btn" onClick={handleSubmit(onSubmit)}>
+        Rent Now
+      </button>
+      <div className="security-info">
+        <div className="security-img">
+          <Security width={32} height={32} />
+        </div>
+        <div className="security-title">
+          <h2>All your data are safe</h2>
+          <div className="security-subinfo">
+            <p>
+              We are using the most advanced security to provide you the best
+              experience ever.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
