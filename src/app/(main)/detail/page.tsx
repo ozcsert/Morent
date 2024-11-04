d26fc7f4ff1c6dfa34baa9e45472107d6be7dc45
fatihsimssek <mm.fatihsimsek@gmail.com>
2024-11-04 06:45:51 +0300

    [SE-21]-Detailed-View filtersidebar eklendi responsive yapıldı

--- src/app/(main)/detail/page.tsx
+++ src/app/(main)/detail/page.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Cardetail from '../../../components/Cardetail/Cardetail'
+import React from 'react';
+import Cardetail from '../../../components/Cardetail/Cardetail';
+import ReviewCard from '@/components/ReviewCard';
+import FilterSidebar from '@/components/FilterSidebar';
+import './styles.scss';
 
 function Detail() {
   return (
-    <div>
-      <Cardetail/>
+    <div className="detail-container">
+      <FilterSidebar />
+      <div className="detail-subcontainer">
+        <Cardetail />
+        <ReviewCard />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Detail
+export default Detail;
