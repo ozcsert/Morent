c3e9e221f9635e47d188a915a56966306508a31f
fatihsimssek <mm.fatihsimsek@gmail.com>
2024-11-04 06:46:25 +0300

    [SE-21] - FİX

--- src/components/Cardetail/Cardetail.tsx
+++ src/components/Cardetail/Cardetail.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import "./Cardetail.scss"
-import ImageBox from './ImageBox'
-import TextBox from './TextBox'
+import React from 'react';
+import './Cardetail.scss';
+import ImageBox from './ImageBox';
+import TextBox from './TextBox';
 
 function Cardetail() {
-
-
   return (
-    <section className='cardetailSection'>
-      <ImageBox/>
-      <TextBox/>
+    <section className="cardetailSection">
+      <ImageBox />
+      <TextBox />
     </section>
-  )
+  );
 }
 
-export default Cardetail
+export default Cardetail;
--- src/components/Cardetail/ImageBox.tsx
+++ src/components/Cardetail/ImageBox.tsx
@@ -1,34 +1,50 @@
-"use client";
+'use client';
 
-import React, { useState } from 'react'
-import "./ImageBox.scss"
+import React, { useState } from 'react';
+import './ImageBox.scss';
 
 import Image from 'next/image';
 
-
 function ImageBox() {
   const [imageIndex, setImageIndex] = useState<number>(0);
-  const carList: string[] = ["/assets/View 1.png", "/assets/View 2.png", "/assets/View 3.png"];
+  const carList: string[] = [
+    '/assets/View 1.png',
+    '/assets/View 2.png',
+    '/assets/View 3.png',
+  ];
   return (
-    <div className='cardetailImageBox'>
-      <div className="cardetailBig" style={{
-        backgroundImage: `url("${carList[imageIndex]}")`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        padding: "25px"
-        ,
-        borderRadius: "10px"
-      }}>
+    <div className="cardetailImageBox">
+      <div
+        className="cardetailBig"
+        style={{
+          backgroundImage: `url("${carList[imageIndex]}")`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          padding: '25px',
+          borderRadius: '10px',
+        }}
+      >
         <div className="cardetailImageTexts" style={{}}>
-
-          <h1 className="cardetailImageHeading">Sports car with the best design and acceleration</h1>
-          <h3 className="cardetailImageText">Safety and comfort while driving a
-            futuristic and elegant sports car</h3>
+          <h1 className="cardetailImageHeading">
+            Sports car with the best design and acceleration
+          </h1>
+          <h3 className="cardetailImageText">
+            Safety and comfort while driving a futuristic and elegant sports car
+          </h3>
         </div>
-
       </div>
       <div className="cardetailSmallContainer">
-        <div className="cardetailSmall" onClick={() => { setImageIndex(0) }} style={imageIndex == 0 ? { border: "2px solid #3563E9", padding: "16px" } : { border: "none" }}>
+        <div
+          className="cardetailSmall"
+          onClick={() => {
+            setImageIndex(0);
+          }}
+          style={
+            imageIndex == 0
+              ? { border: '2px solid #3563E9', padding: '16px' }
+              : { border: 'none' }
+          }
+        >
           <Image
             src={carList[0]}
             alt="Car"
@@ -41,7 +57,17 @@ function ImageBox() {
             height={132}
           />
         </div>
-        <div className="cardetailSmall" style={imageIndex == 1 ? { border: "2px solid #3563E9", padding: "1vw" } : { border: "none" }} onClick={() => { setImageIndex(1) }}>
+        <div
+          className="cardetailSmall"
+          style={
+            imageIndex == 1
+              ? { border: '2px solid #3563E9', padding: '1vw' }
+              : { border: 'none' }
+          }
+          onClick={() => {
+            setImageIndex(1);
+          }}
+        >
           <Image
             src={carList[1]}
             alt="Direction"
@@ -52,10 +78,19 @@ function ImageBox() {
             }}
             width={148}
             height={132}
-
           />
         </div>
-        <div className="cardetailSmall" style={imageIndex == 2 ? { border: "2px solid #3563E9", padding: "16px" } : { border: "none" }} onClick={() => { setImageIndex(2) }}>
+        <div
+          className="cardetailSmall"
+          style={
+            imageIndex == 2
+              ? { border: '2px solid #3563E9', padding: '16px' }
+              : { border: 'none' }
+          }
+          onClick={() => {
+            setImageIndex(2);
+          }}
+        >
           <Image
             src={carList[2]}
             alt="Seat"
@@ -66,12 +101,11 @@ function ImageBox() {
             }}
             width={148}
             height={132}
-
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ImageBox
+export default ImageBox;
